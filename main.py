912a0934f878de7879a028c84e6a96760cfb93d1
paulus <paulusojak@gmail.com>
2025-04-03 16:07:31 +0700

    Add player's default shape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
@@ -8,13 +9,19 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clk = pygame.time.Clock()
+    dt = 0
+    test = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
 
     while(1):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
+        test.draw(screen)
         pygame.display.flip()
+        dt = game_clk.tick(60) / 1000
 
 
 if __name__ == "__main__":
